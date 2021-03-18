a66a8ca11483f2bb6b3566b4c431e6114e95d280
Rajneesh Rai (Contractor) <RRai@mansfieldoil.com>
2021-03-18 19:43:21 +0530

    anagram

---
+++ anagram.md
@@ -0,0 +1,49 @@
+# Anagram example
+
+```
+# javscript
+
+let str1 = "abcdf";
+let str2 = "dcbaa";
+let isAnagram = (a, b) => {
+  let isAnagram = true;
+  let dict = {};
+  let arrOfstr1 = a.split("");
+  let arrOfstr2 = b.split("");
+  for (let index = 0; index < arrOfstr1.length; index++) {
+    if (dict[arrOfstr1[index]]) {
+      dict[arrOfstr1[index]] = dict[arrOfstr1[index]] + 1;
+    } else {
+      dict[arrOfstr1[index]] = 1;
+    }
+  }
+
+  for (let index = 0; index < arrOfstr2.length; index++) {
+    if (dict[arrOfstr2[index]]) {
+      const s = dict[arrOfstr2[index]] - 1;
+      if (s < 0) {
+        isAnagram = false;
+        break;
+      }
+      dict[arrOfstr2[index]] = s;
+    } else {
+      isAnagram = false;
+      break;
+    }
+  }
+
+  for (const key in dict) {
+    if (Object.hasOwnProperty.call(dict, key)) {
+      if (dict[key] != 0) {
+        isAnagram = false;
+        break;
+      }
+    }
+  }
+
+  return isAnagram;
+};
+
+console.log(isAnagram(str1, str2));
+
+```
--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-let str = 'i love javascript';
 
-let fStr = '';
-for(let i=str.length-1;i>=0;i--){
-fStr = fStr+ str[i];
-}
+let str1 = "abcdf";
+let str2 = "dcbaa";
+let isAnagram = (a, b) => {
+  let isAnagram = true;
+  let dict = {};
+  let arrOfstr1 = a.split("");
+  let arrOfstr2 = b.split("");
+  for (let index = 0; index < arrOfstr1.length; index++) {
+    if (dict[arrOfstr1[index]]) {
+      dict[arrOfstr1[index]] = dict[arrOfstr1[index]] + 1;
+    } else {
+      dict[arrOfstr1[index]] = 1;
+    }
+  }
 
-console.log(str.split('').reverse().join(''));
-console.log(fStr)
-fStr = ''
-var s = str.split(' ');
-for(let i=s.length-1;i >=0;i--){
-    fStr = fStr+ s[i] + ' ';
-}
+  for (let index = 0; index < arrOfstr2.length; index++) {
+    if (dict[arrOfstr2[index]]) {
+      const s = dict[arrOfstr2[index]] - 1;
+      if (s < 0) {
+        isAnagram = false;
+        break;
+      }
+      dict[arrOfstr2[index]] = s;
+    } else {
+      isAnagram = false;
+      break;
+    }
+  }
 
-console.log(fStr.trim())
+  for (const key in dict) {
+    if (Object.hasOwnProperty.call(dict, key)) {
+      if (dict[key] != 0) {
+        isAnagram = false;
+        break;
+      }
+    }
+  }
+
+  return isAnagram;
+};
+
+console.log(isAnagram(str1, str2));
